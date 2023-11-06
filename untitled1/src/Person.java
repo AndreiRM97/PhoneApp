976956e17f4d9988138c92eebc155d4a374278f6
Andrei M <zenkaken123@gmail.com>
2023-11-06 21:32:33 +0200

    a

---
+++ untitled1/src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    private String nume;
+    private int salariu;
+
+    public Person(String nume, int salariu) {
+        this.nume = nume;
+        this.salariu = salariu;
+    }
+
+    public String getName() {
+        return nume;
+    }
+
+    public double getSalariu() {
+        return salariu;
+    }
+}
+
